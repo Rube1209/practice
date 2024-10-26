cb66f3fadbd388e82770180c246439a115de89d6
Rube1209 <linyi_111@outlook.com>
2024-10-26 08:58:30 +0800

    Update Climb_stairs.rs

--- rust/dynamic_programming/Climb_stairs.rs
+++ rust/dynamic_programming/Climb_stairs.rs
@@ -1,1 +1,20 @@
+fn climb_stairs(n: i32) -> i32 {
+    if n == 1 {
+        return 1;
+    } else if n == 2 {
+        return 2;
+    }
+
+    let mut first: i32 = 1;
+    let mut second: i32 = 2;
+    let mut ways: i32 = 0;
+
+    for i in 3..=n {
+        ways = first + second;
+        first = second;
+        second = ways;
+    }
+    
+    return ways;
+}
 
